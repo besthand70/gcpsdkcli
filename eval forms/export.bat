359bbf78cf3ed1038a968ef55cc575d1813413b9
besthand70 <berktan.bestel@hotmail.com>
2023-06-11 12:52:59 +0300

    synced after working with Bela

---
+++ eval forms/export.bat
@@ -0,0 +1,13 @@
+echo exporting 11 QM evaluation forms from hsbc-devorg 
+gc quality forms evaluations get 743efaf5-8119-4c8d-a9fd-ef55c9faee72 --profile hsbc-devorg >"743efaf5-8119-4c8d-a9fd-ef55c9faee72_GPS Offshore Call Scoring Form.json"
+gc quality forms evaluations get 8d205913-711c-4a8f-a19a-879954529901 --profile hsbc-devorg >"8d205913-711c-4a8f-a19a-879954529901_GPS Offshore Escalation Evaluation Scoring.json"
+gc quality forms evaluations get c1a54037-8d2b-483b-97e0-31bf77bf006e --profile hsbc-devorg >"c1a54037-8d2b-483b-97e0-31bf77bf006e_GTRF Invoice Discounting.json"
+gc quality forms evaluations get dbbff453-02c0-461c-96e3-dcfdd90cb8ce --profile hsbc-devorg >"dbbff453-02c0-461c-96e3-dcfdd90cb8ce_GTRF Trade.json"
+gc quality forms evaluations get b176171c-a105-4d4e-a374-1d2f4fd3e87f --profile hsbc-devorg >"b176171c-a105-4d4e-a374-1d2f4fd3e87f_HSBC Net Helpdesk Call Coaching Sheet.json"
+gc quality forms evaluations get a1f64c5e-bac5-4259-8e61-41e5f7347540 --profile hsbc-devorg >"a1f64c5e-bac5-4259-8e61-41e5f7347540_Remediation 10% Complaints Quality Check .json"
+gc quality forms evaluations get ac1d31d5-a72d-4ead-a2cd-50a3d471417f --profile hsbc-devorg >"ac1d31d5-a72d-4ead-a2cd-50a3d471417f_Remediation Additional Call Grading Form.json"
+gc quality forms evaluations get d8832bf8-fd50-4c12-bca7-cfb02bea0425 --profile hsbc-devorg >"d8832bf8-fd50-4c12-bca7-cfb02bea0425_Remediation Call Grading Form.json"
+gc quality forms evaluations get 2ea97365-84a2-426f-abfc-362cba9622c7 --profile hsbc-devorg >"2ea97365-84a2-426f-abfc-362cba9622c7_Remediation Complaint Diary Watch Check.json"
+gc quality forms evaluations get 273bdc89-8ce6-42ad-bea4-f1c9a4c567da --profile hsbc-devorg >"273bdc89-8ce6-42ad-bea4-f1c9a4c567da_Remediation UBO 10% Quality Check .json"
+gc quality forms evaluations get 14d33e25-ed77-45e5-b6c7-f7026c990990 --profile hsbc-devorg >"14d33e25-ed77-45e5-b6c7-f7026c990990_Remediation UBO Audit Check .json"
+
